d99a9f52c1e842ae7bf792ed8aacbbdfd3048389
Vinicius <vinicius@vkohler.com.br>
2023-07-11 16:00:20 -0300

    feat: signout

--- app/src/main/java/com/vkohler/wealthtracker/activities/MainActivity.java
+++ app/src/main/java/com/vkohler/wealthtracker/activities/MainActivity.java
@@ -3,11 +3,16 @@ package com.vkohler.wealthtracker.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.vkohler.wealthtracker.databinding.ActivityMainBinding;
 import com.vkohler.wealthtracker.utilities.Constants;
 import com.vkohler.wealthtracker.utilities.PreferenceManager;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         preferenceManager = new PreferenceManager(getApplicationContext());
         setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.signOut.setOnClickListener(v -> {
+            preferenceManager.clear();
+            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+            finish();
+        });
     }
 }
