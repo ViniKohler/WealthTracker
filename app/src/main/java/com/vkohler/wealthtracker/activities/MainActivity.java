605379b886e218ce0ea2f7055ea7d312b37d0fa1
ViniKohler <vinicius@vkohler.com.br>
2023-07-19 15:51:49 -0300

    feat: home activity

--- app/src/main/java/com/vkohler/wealthtracker/activities/MainActivity.java
+++ app/src/main/java/com/vkohler/wealthtracker/activities/MainActivity.java
@@ -1,10 +1,14 @@
 package com.vkohler.wealthtracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.text.InputType;
 
+import com.vkohler.wealthtracker.R;
 import com.vkohler.wealthtracker.databinding.ActivityMainBinding;
 import com.vkohler.wealthtracker.utilities.Constants;
 import com.vkohler.wealthtracker.utilities.PreferenceManager;
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     PreferenceManager preferenceManager;
     ActivityMainBinding binding;
+    boolean isBalanceVisible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
         });
         binding.name.setOnClickListener(v -> {
             startActivity(new Intent(getApplicationContext(), MyProfileActivity.class));
+        });
+        binding.eye.setOnClickListener(v -> {
+            if (isBalanceVisible) {
+                binding.visibility.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.red)));
+                binding.eye.setColorFilter(ContextCompat.getColor(this, R.color.red));
+                binding.balance.setTextColor(getResources().getColor(R.color.text_secondary));
+                binding.currency.setTextColor(getResources().getColor(R.color.text_secondary));
+                binding.balance.setText("0000");
+                binding.balance.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                isBalanceVisible = false;
+            } else {
+                binding.visibility.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.green)));
+                binding.eye.setColorFilter(ContextCompat.getColor(this, R.color.green));
+                binding.balance.setTextColor(getResources().getColor(R.color.text_primary));
+                binding.currency.setTextColor(getResources().getColor(R.color.text_primary));
+                binding.balance.setInputType(InputType.TYPE_CLASS_TEXT);
+                binding.balance.setText("0.00");
+                isBalanceVisible = true;
+            }
         });
     }
 
