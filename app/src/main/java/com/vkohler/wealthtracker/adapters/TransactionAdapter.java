4d12212277f1173bd080fb518aa5082edc2fb04e
Vinicius <vinicius@vkohler.com.br>
2023-10-10 14:55:19 -0300

    feat: colors for transactions

--- app/src/main/java/com/vkohler/wealthtracker/adapters/TransactionAdapter.java
+++ app/src/main/java/com/vkohler/wealthtracker/adapters/TransactionAdapter.java
@@ -3,14 +3,17 @@ package com.vkohler.wealthtracker.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.vkohler.wealthtracker.R;
 import com.vkohler.wealthtracker.models.Transaction;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
@@ -33,6 +36,14 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
         holder.title.setText(transaction.getTitle());
         holder.category.setText(transaction.getCategory());
         holder.value.setText(String.valueOf(transaction.getValue()));
+        int background;
+        int temp = new BigDecimal(transaction.getValue().toString()).compareTo(BigDecimal.ZERO);
+        if (temp == 1) {
+            background = R.drawable.category_green_background;
+        } else {
+            background = R.drawable.category_red_background;
+        }
+        holder.categoryIcon.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(), background));
     }
 
     @Override
@@ -41,14 +52,14 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     }
 
     public class TransactionViewHolder extends RecyclerView.ViewHolder {
-        public TextView title;
-        public TextView category;
-        public TextView value;
+        public TextView title, category, value;
+        public ImageView categoryIcon;
 
         public TransactionViewHolder(View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.title);
             category = itemView.findViewById(R.id.category);
+            categoryIcon = itemView.findViewById(R.id.categoryIcon);
             value = itemView.findViewById(R.id.value);
         }
     }
